4b04d86ad0c3255e8b69bdde399284491920b923
Val Rakitine <vrakitine@gmail.com>
2023-08-29 15:26:19 -0400

    Added  const gotoHomePage

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,14 @@
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 export default function Home() {
+
+  const router = useRouter();
+  const gotoHomePage = () => {
+    router.push('/');
+  };
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -8,6 +16,7 @@ export default function Home() {
           Get started by editing DASHBOARD&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
         </p>
+        <h3><button onClick={gotoHomePage}>Go to Home Page</button></h3>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
