40314d01fabc7cfd09de7f1cddd893167067aaa3
Val Rakitine <vrakitine@gmail.com>
2023-08-30 10:27:08 -0400

    Added Header component

---
+++ app/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div>
+      <h3>---------- Val's header -------</h3>
+    </div>
+  )
+}
--- app/dashboard/settings/page.tsx
+++ app/dashboard/settings/page.tsx
@@ -1,20 +1,29 @@
-import Image from 'next/image';
 import Link from 'next/link';
+import React from 'react';
 
-export default function HiPeter() {
+import Image from 'next/image';
+//import { useRouter } from 'next/router';
+
+export default function Home() {
+
+  //const router = useRouter();
+  //const gotoHomePage = () => {
+    //router.push('/');
+  //};
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing 333333&nbsp;
+          Get started by editing SETTINGS&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
         </p>
-        <Link href="/dashboard">
-        <a>[Go to Dashboard Page]</a>
+        <Link href="/">
+        <a>[Go to Home Page]</a>
       </Link>
-      <Link href="/dashboard/settings">
-        <a>[Go to Settings Page]</a>
-      </Link>
+      
+
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
